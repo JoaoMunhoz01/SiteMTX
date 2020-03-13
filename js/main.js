1e6b87ff37fc27204978cccbe95b6b4a5bdc39ac
JoaoMunhoz01 <joao.victormunhoz@hotmail.com>
2020-03-13 09:18:29 -0400

    version 1.1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $('.slider-principal').slick({
     autoplaySpeed: 3000
     
 });
+  $('.slider-news').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+    
+  });
+ 
