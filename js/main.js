8a608831f77d959cdc9cd6259b3457fac84e0bbd
JoaoMunhoz01 <joao.victormunhoz@hotmail.com>
2020-03-17 15:27:51 -0400

    version 1.2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-$('.slider-principal').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 3000
-    
+$(".slider-principal").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000
 });
-  $('.slider-news').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 3000
-    
-  });
- 
+$(".slider-news").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
